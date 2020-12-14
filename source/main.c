4412385e193da010480bac558032b6ac614c3363
Ubuntu <cloeza1@mail.ccsf.edu>
2020-12-13 19:25:46 -0800

    Working on part 1: compiles but does not work

--- source/main.c
+++ source/main.c
@@ -1,8 +1,6 @@
 
 
 
-
-
 /*	Author: lab
  *  Partner(s) Name: 
  *	Lab Section:
@@ -15,14 +13,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "time.h"
-#include "usart.h"
+#include "usart_1284.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 unsigned char temp;
-
-
 enum MC1_States{MC1_Transmit, MC1_Wait, MC1_Receive} MC1_State;
 
 void Micro1(){
@@ -30,7 +26,7 @@ void Micro1(){
     // Transition
     switch(MC1_State){
 	case MC1_Transmit:
-	    if(USART_IsSendReady()){
+	    if(USART_IsSendReady(0)){
 		MC1_State = MC1_Wait;
 	    }else{
 		MC1_State = MC1_Transmit;
@@ -38,7 +34,7 @@ void Micro1(){
 	    break;
 
 	case MC1_Wait:
-	    if(USART_HasTransmitted()){
+	    if(USART_HasTransmitted(0)){
 		MC1_State = MC1_Receive;
 	    } else { 
 		MC1_State = MC1_Wait;
@@ -46,7 +42,7 @@ void Micro1(){
 	    break;
 
 	case MC1_Receive:
-	    if(USART_HasReceived()){
+	    if(USART_HasReceived(0)){
 		MC1_State = MC1_Transmit;
 	    } else { 
 		MC1_State = MC1_Receive;
@@ -57,13 +53,14 @@ void Micro1(){
     // Action
     switch(MC1_State){
 	case MC1_Transmit:
-	    USART_Send(temp);
+	    USART_Send(temp, 0);
 	    break;
-	
+	case MC1_Wait:
+	    break;
 	case MC1_Receive:
-	    temp = USART_Received();
-	    PINA = temp;
-	    USART_Flush();	    
+	    temp = USART_Receive(0);
+	    PINA = 1;
+	    USART_Flush(0);	    
 	    break;
 
     }
@@ -72,15 +69,16 @@ void Micro1(){
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
-    DDRD = 0xFF; PORTD = 0xFF;
+    DDRD = 0xFF; PORTD = 0x00;
     temp = 0xFF;
   
+    TimerSet(1000);
+    TimerOn();
     MC1_State = MC1_Transmit;
     initUSART(0);
     /* Insert your solution below */
-    while (1) {
-   	
-   	Micro1();
+    while (1) {   	
+  	Micro1();
     }
     return 1;
 }
